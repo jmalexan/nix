c701a345e777b73e6e92f91f8aee4571ef1cc9a2
Jonathan Alexander <me@jmalexan.com>
2026-06-04 15:31:29 -0400

    Add tmux

--- home/linux.nix
+++ home/linux.nix
@@ -3,4 +3,19 @@
 {
   imports = [ ./common.nix ];
 
+  programs.tmux = {
+    enable = true;
+    terminal = "tmux-256color";
+  };
+
+  # Auto-attach (or create) a tmux session named "main" on SSH login.
+  programs.fish.interactiveShellInit = ''
+    if status is-interactive
+        and set -q SSH_CONNECTION
+        and not set -q TMUX
+        and not set -q INSIDE_EMACS
+        and not string match -q 'screen*' -- $TERM
+        exec tmux new-session -A -s main
+    end
+  '';
 }
